b7bbd9498c9c24e2bf0264fc825e2a6b1dc42b2e
Antonio Griffith-Keaton <antoniogriffith2@yahoo.com>
2021-04-06 20:09:08 -0400

    Tested Price Formatted Logic

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -4,4 +4,8 @@ from app.shopping import format_usd
 # TODO: test the code
 
 def test_format_usd():
-    assert 2 == 2
+    assert format_usd(9.5) == "$9.50"
+
+    #edge cases 
+    assert format_usd(100000000) == "$100,000,000.00"
+    assert format_usd(0.25) == "$0.25"
